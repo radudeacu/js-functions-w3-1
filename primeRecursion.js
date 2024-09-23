67055fb3d72ab2a1bf134f369a5f93c9c757c723
radudeacu <deacuradu@gmail.com>
2024-09-23 23:37:45 +0300

    prime check using recursion

---
+++ primeRecursion.js
@@ -0,0 +1,2 @@
+// Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not using recursion.
+// Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
