88c2b50e76e3d69760120ed9a65a77ef6f75ff47
radudeacu <deacuradu@gmail.com>
2024-09-24 18:11:23 +0300

    function that accepts an argument and returns its type

---
+++ argumentType.js
@@ -0,0 +1,2 @@
+// Write a JavaScript function that accepts an argument and returns the type.
+// Note : There are six possible values that typeof returns: object, boolean, function, number, string, and undefined.
