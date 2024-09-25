ecb191a8a5c2420d1bfb991688a6e94fa79614fc
radudeacu <deacuradu@gmail.com>
2024-09-25 13:49:09 +0300

    implemented funct that returns a str in alphabetical order

--- aplhabeticalOrder.js
+++ aplhabeticalOrder.js
@@ -2,3 +2,16 @@
 // Example string : 'webmaster'
 // Expected Output : 'abeemrstw'
 // Assume punctuation and numbers symbols are not included in the passed string.
+
+function alphabetString(str) {
+    return str.split('').sort().join('');
+}
+
+// Example
+console.log(alphabetString('mango')); // agmno
+
+
+
+// str.split(''): This splits the input string into an array of individual characters.
+// .sort(): This sorts the array of characters. By default, JavaScript's sort() method sorts strings alphabetically.
+// .join(''): This joins the sorted array back into a string.
